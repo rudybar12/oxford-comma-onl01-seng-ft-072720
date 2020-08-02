4c043731b7bf4486f1d76d887e96c5d1caff87de
Rodolfo Barrientos <rudybar12@yahoo.com>
2020-08-02 08:37:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
  if array.size > 2
    last = array.pop
-   output = array.join
+   output = array.join(", ")
    output = output + " , and " + last
  else
    output = array.join(" and ")
